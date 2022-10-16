f051e1ecf18031cfebf8f9d4d8fb672058cadaf5
Javier Guerrero Carmona <fjguerreroc@icloud.com>
2022-10-17 00:28:47 +0200

    Add topTracks to artist view

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -10,16 +10,30 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 export class ArtistComponent {
 
   artist: any = {};
+  toptracks: any[] = [];
   loading: boolean = true;
 
   constructor( private activatedRoute: ActivatedRoute, private spotifyService:SpotifyService) { 
-    this.activatedRoute.params.subscribe(data => {
-      const id = data['id'];
-      this.spotifyService.getArtist(id).subscribe(artist => {
-        this.artist = artist;
-        this.loading = false;
-      });
+    
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'];
+      this.getArtist(id);
+      this.getTopTracks(id); 
     })
+  }
+
+  getArtist(id: string) {
+    this.spotifyService.getArtist(id).subscribe(artist => {
+      this.artist = artist;
+      this.loading = false;
+    });
+  }
+
+  getTopTracks(id: string) {
+    this.spotifyService.getTopTracks(id).subscribe(topTracks => {
+      this.toptracks = topTracks;
+      console.log(this.toptracks);
+    });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -34,4 +34,9 @@ export class SpotifyService {
   getArtist(id: string) {
     return this.getQuery(`artists/${ id }`);
   }
+
+  getTopTracks(id: string) {
+    return this.getQuery(`artists/${ id }/top-tracks?country=us`)
+      .pipe(map((data:any) => data['tracks']));
+  }
 }
