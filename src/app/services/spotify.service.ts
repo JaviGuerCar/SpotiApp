cbb526d2a136a280868c4d272c9fa8636265f78c
Javier Guerrero Carmona <fjguerreroc@icloud.com>
2022-10-16 02:59:26 +0200

    Changes on service

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { credentials } from 'src/environments/credentials';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      'Authorization':'Bearer BQApPv-6jZiYsq8hdhdVA3mhUSu76w2CaOR5fkko8wtpiiePu1rMM_g1oLzafQWVTqBAQGcNQjFmYvNVKEPdh_1j4Zpqg-q5MXUQXQsBY2ojXQh0WlI'
+      'Authorization': credentials.token
     });
 
     return this.http.get(url, { headers });
